661e06026dd66da6cde7bada4ebf05ff48a56324
„tassilomorino“ <tassilomorino@gmail.com>
2021-08-27 13:45:13 +0200

    added cypress folder system

---
+++ cypress/integration/WIP/login_with_scenarios.spec.js
@@ -0,0 +1,33 @@
+/** @format */
+
+const authUser = require("../fixtures/auth-user.json");
+
+describe("The Login Page", () => {
+  const { email, password, handle } = authUser;
+  before(() => {
+    cy.setCookie("Cookie_settings", "all");
+    cy.visit("/");
+  });
+
+  // data-cy tags were added to the html as necessary
+  it("logs User in successfully", () => {
+    cy.get(".FilterComponent")
+      .find("[data-cy=open-signnote]")
+      .click({ force: true });
+
+    //ADD SCENARIOS ABOVE THE HAPPY PATH
+
+    // click on login / anmelden
+    cy.get("[data-cy=login]").click();
+    // fill email and password
+    cy.get("[data-cy=login-email]").click().type(email);
+    cy.get("[data-cy=login-password]").click().type(password);
+    // submit button
+    cy.get("[data-cy=login-user]").click();
+    // click the Profile button in Sidebar
+    cy.get(".FilterComponent")
+      .find("[data-cy=profile-button]")
+      .click({ force: true });
+    cy.get("[data-cy=hey-user]").should("contain.text", "Hey " + handle);
+  });
+});
