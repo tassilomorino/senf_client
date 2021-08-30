74d1f9bfcc273660f28312e0468b2b94d297d33c
„tassilomorino“ <tassilomorino@gmail.com>
2021-08-30 15:26:53 +0200

    started with post_delete_idea instructions

--- cypress/integration/ToDo/post_delete_idea.spec.js
+++ cypress/integration/ToDo/post_delete_idea.spec.js
@@ -7,6 +7,15 @@ describe("Testing posting and deleting an idea", () => {
   });
 
   it("inspecting if posting an idea is working fine, then if you can  also delete it", () => {
-    //Component for adding an idea is not working correcctly at the moment. Geocoder needs to be fixed
+    //PR IS IN THE PIPELINE TO FIX ISSUES ABOUT THE GEOCODER
+    //FOR THIS TEST THE LOGIN PROCESS NEEDS TO BE DONE FIRST...
+    //click on the button "+ New idea" within sidebar
+    //cy
+    //Check if ...?
+    //cy
+    //Switch between projects and check if that is working fine
+    //cy
+    //Switch between projects and check if that is working fine
+    //cy
   });
 });
