4c2cc321233adf298aacbed1a051d0b9b56d4b27
„tassilomorino“ <tassilomorino@gmail.com>
2021-08-31 13:35:18 +0200

    addedd post idea test instructions

--- cypress/integration/ToDo/post_delete_idea.spec.js
+++ cypress/integration/ToDo/post_delete_idea.spec.js
@@ -15,7 +15,47 @@ describe("Testing posting and deleting an idea", () => {
     //cy
     //Switch between projects and check if that is working fine
     //cy
-    //Switch between projects and check if that is working fine
+    //Move the Map
+    //cy
+    //Check if address is changing in the address-bar
+    //cy
+    //Click into address-bar
+    //cy
+    //Type for an eg. "Rennebergstr" and select the first suggestion and hit enter
+    //cy
+    //Check if map moves to address
+    //cy
+    //Check if address is updating in the address-bar
+    //cy
+    //Click button "Confirm location"
+    //cy
+    //Check if address from address-bar is also in the card
+    //cy
+    //Click the selectLocationContainer to go back
+    //cy
+    //Click button "No location"
+    //cy
+    //Check if address in the card is updated to "Without Location"
+    //cy
+    //Check if the submitButton is disabled (it shall be disabled)
+    //cy
+    //Click into input field title
+    //cy
+    //Type for an eg. "Test-idea from Cypress"
+    //cy
+    //Check if the submitButton is disabled (it shall be disabled)
+    //cy
+    //Click into input field body
+    //cy
+    //Type for an eg. "Test-Body from Cypress"
+    //cy
+    //Check if the submitButton is disabled (it shall not be disabled)
+    //cy
+    //Select topic and check if that is working
+    //cy
+    //Click the submitButton
+    //cy
+    //Check if you are being redirected to the idea you just created
     //cy
   });
 });
