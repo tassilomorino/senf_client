ffc87013c0c28db227f651f03cac357fbde2f313
Arc_Anya <arcanyatwitch@gmail.com>
2021-09-03 13:21:27 +0200

    Modified test

--- cypress/integration/ToDo/post_delete_idea.spec.js
+++ cypress/integration/ToDo/post_delete_idea.spec.js
@@ -1,5 +1,12 @@
 /** @format */
 
+    // overall logic and tests to be done:
+    // 1) NOT LOGGED IN 
+    // 1.a) post an idea => fail
+    // 2) LOG IN
+    // 2.a) post an idea => success
+    // 2.b) delete his own idea => success
+
 const { before } = require("lodash");
 
 describe("User that wants to post or delete an idea", () => {
@@ -15,17 +22,7 @@ describe("User that wants to post or delete an idea", () => {
 
     });
 
-    it("tries to delete an idea", () => {
 
-    });
-
-    // overall logic and tests to be done:
-    // 1) NOT LOGGED IN 
-    // 1.a) post an idea => fail
-    // 1.b) delete an idea => fail
-    // 2) LOG IN
-    // 2.a) post an idea => success
-    // 2.b) delete an idea => success
 
     describe("and is logged in", () => {
 
@@ -34,8 +31,8 @@ describe("User that wants to post or delete an idea", () => {
       })
 
       it("posts an idea", () => {
-        //PR IS IN THE PIPELINE TO FIX ISSUES ABOUT THE GEOCODER
-        //FOR THIS TEST THE LOGIN PROCESS NEEDS TO BE DONE FIRST...
+        
+        
         //click on the button "+ New idea" within sidebar
         //cy
         //Check if ...?
@@ -64,11 +61,8 @@ describe("User that wants to post or delete an idea", () => {
         //cy
         //Check if address in the card is updated to "Without Location"
         //cy
-        //Check if the submitButton is disabled (it shall be disabled)
+        //Check if the submitButton is disabled (it shall be disabled because no title/body yet)
         //cy
-
-        // ***QUESTION: maybe there are projects without a specific location? e.g. incentive to have plants on balcony, or adding chargers for eBikes...
-
         //Click into input field title
         //cy
         //Type for an eg. "Test-idea from Cypress"
