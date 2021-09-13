5286da2ac8e4ea845c4443e3fa68d452fd1dc21c
„tassilomorino“ <tassilomorino@gmail.com>
2021-09-14 00:09:01 +0200

    cleaning up topbar

--- src/components/layout/Topbar.js
+++ src/components/layout/Topbar.js
@@ -7,7 +7,7 @@ import PropTypes from "prop-types";
 import InlineInformationPage from "../infocomponents/InlineInformationPage";
 
 //REDUX
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 
 //COMPONENTS
 import SignNote from "../profile/SignNote";
@@ -23,72 +23,57 @@ import Tabs from "../module/Tabs";
 import { MenuData } from "../../data/MenuData";
 import ScrollTabs from "../module/ScrollTabs";
 
-class Topbar extends React.Component {
-  render() {
-    const {
-      user: { authenticated },
-      order,
-      handleClick,
-      handleTopicSelector,
-      topicsSelected,
-      deleteAccount,
-      handleLogout,
-    } = this.props;
-    const { loading } = this.props.data;
+const Topbar = ({
+  order,
+  handleClick,
+  handleTopicSelector,
+  topicsSelected,
+  deleteAccount,
+  handleLogout,
+}) => {
+  const { loading } = useSelector((state) => state.data);
+  const { authenticated } = useSelector((state) => state.user);
 
-    const sign = !authenticated ? (
-      <div className="profile">
-        <SignNote />
-        <img src={Noprofile} width="35" alt="profilePlaceHolderImage" />
+  return (
+    !loading &&
+    (order === 1 || order === 2) && (
+      <div className="TopNav">
+        <InlineInformationPage />
+
+        {!authenticated ? (
+          <div className="profile">
+            <SignNote />
+            <img src={Noprofile} width="35" alt="profilePlaceHolderImage" />
+          </div>
+        ) : (
+          <div className="profile">
+            <Account
+              handleTopicSelector={handleTopicSelector}
+              topicsSelected={topicsSelected}
+              deleteAccount={deleteAccount}
+              handleLogout={handleLogout}
+            />
+            <img src={profile_yellow} width="35" alt="profileImage" />
+          </div>
+        )}
+        <h1 className="logo1">
+          <img src={Logo} width="100px"></img>
+        </h1>
+        <ScrollTabs
+          loading={loading}
+          handleClick={handleClick}
+          order={order}
+          tabLabels={MenuData.map((item) => item.text)}
+          marginTop={"67px"}
+          marginBottom={"0px"}
+          lineColor={"#c9c9c9"}
+        ></ScrollTabs>
       </div>
-    ) : (
-      <div
-        className="profile"
-        // onClick={() => handleClick(4)}
-      >
-        <Account
-          handleTopicSelector={handleTopicSelector}
-          topicsSelected={topicsSelected}
-          deleteAccount={deleteAccount}
-          handleLogout={handleLogout}
-        />
-        <img src={profile_yellow} width="35" alt="profileImage" />
-      </div>
-    );
-
-    const menu =
-      order === 1 || (order === 2 && !loading) ? (
-        <div className="TopNav">
-          <InlineInformationPage />
-
-          {sign}
-          <h1 className="logo1">
-            <img src={Logo} width="100px"></img>
-          </h1>
-          <ScrollTabs
-            loading={loading}
-            handleClick={handleClick}
-            order={order}
-            tabLabels={MenuData.map((item) => item.text)}
-            marginTop={"67px"}
-            marginBottom={"0px"}
-            lineColor={"#c9c9c9"}
-          ></ScrollTabs>
-        </div>
-      ) : null;
-
-    return menu;
-  }
-}
+    )
+  );
+};
 Topbar.propTypes = {
   user: PropTypes.object.isRequired,
 };
 
-const mapActionsToProps = {};
-
-const mapStateToProps = (state) => ({
-  data: state.data,
-  user: state.user,
-});
-
-export default connect(mapStateToProps, mapActionsToProps)(Topbar);
+export default Topbar;
