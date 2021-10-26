0d2223cba0d610dbd8d72a3c9e805af4c6a8d5b5
„tassilomorino“ <tassilomorino@gmail.com>
2021-10-26 21:30:58 +0200

    added sidebarmenu-items hover state with expandbutton

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -7,3 +7,24 @@ export const Text = styled.p`
   line-height: 22.666px;
   color: #353535;
 `;
+
+export const SideBarTabs = styled.div`
+  position: relative;
+  overflow: hidden;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  text-align: left;
+
+  width: 160px;
+  height: 35px;
+  left: 20px;
+  top: 80px;
+  font-size: 14pt;
+  line-height: 14pt;
+  color: #353535;
+
+  margin-bottom: 10px;
+  border-radius: 17.5px;
+`;
