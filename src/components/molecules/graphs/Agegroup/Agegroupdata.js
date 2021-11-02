927607a7819088b39cd8b22467db5cf8e0507d8a
„tassilomorino“ <tassilomorino@gmail.com>
2021-11-02 15:37:10 +0100

    agegroupsgraph is now correct

--- src/components/molecules/graphs/Agegroup/Agegroupdata.js
+++ src/components/molecules/graphs/Agegroup/Agegroupdata.js
@@ -3,133 +3,148 @@
 import React from "react";
 
 export function Agegroupdata(screams, likes, filteredTopic) {
-  const listOfIdsPerTopic1 = [];
+  // const listOfIdsPerTopic1 = [];
 
-  for (const scream of screams) {
-    if (scream.Thema === filteredTopic) {
-      listOfIdsPerTopic1.push(scream.id);
-    }
-  }
+  // for (const scream of screams) {
+  //   if (scream.Thema === filteredTopic) {
+  //     listOfIdsPerTopic1.push(scream.id);
+  //   }
+  // }
 
-  const votesTopic1agegroupUnder18 = likes.reduce((acc, cur) => {
-    // cur refers to each object in the array votes & acc is a number that get returned
+  // const votesTopic1agegroupUnder18 = likes.reduce((acc, cur) => {
+  //   // cur refers to each object in the array votes & acc is a number that get returned
 
-    if (
-      listOfIdsPerTopic1.includes(cur.ideaId) &&
-      cur.age >= 14 &&
-      cur.age < 18
-    ) {
-      return ++acc;
-    } else {
-      return acc;
-    }
-  }, 0); // here, 0 is the initiual number for the accumulator
+  //   if (
+  //     listOfIdsPerTopic1.includes(cur.ideaId) &&
+  //     cur.age >= 14 &&
+  //     cur.age < 18
+  //   ) {
+  //     return ++acc;
+  //   } else {
+  //     return acc;
+  //   }
+  // }, 0); // here, 0 is the initiual number for the accumulator
 
-  const votesTopic1agegroupBetween18and24 = likes.reduce((acc, cur) => {
-    if (
-      listOfIdsPerTopic1.includes(cur.ideaId) &&
-      cur.age >= 18 &&
-      cur.age < 25
-    ) {
-      return ++acc;
-    } else {
-      return acc;
-    }
-  }, 0); // here, 0 is the initiual number for the accumulator
+  // const votesTopic1agegroupBetween18and24 = likes.reduce((acc, cur) => {
+  //   if (
+  //     listOfIdsPerTopic1.includes(cur.ideaId) &&
+  //     cur.age >= 18 &&
+  //     cur.age < 25
+  //   ) {
+  //     return ++acc;
+  //   } else {
+  //     return acc;
+  //   }
+  // }, 0); // here, 0 is the initiual number for the accumulator
 
-  const votesTopic1agegroupBetween25and34 = likes.reduce((acc, cur) => {
-    if (
-      listOfIdsPerTopic1.includes(cur.ideaId) &&
-      cur.age >= 25 &&
-      cur.age < 34
-    ) {
-      return ++acc;
-    } else {
-      return acc;
-    }
-  }, 0); // here, 0 is the initiual number for the accumulator
+  // const votesTopic1agegroupBetween25and34 = likes.reduce((acc, cur) => {
+  //   if (
+  //     listOfIdsPerTopic1.includes(cur.ideaId) &&
+  //     cur.age >= 25 &&
+  //     cur.age < 34
+  //   ) {
+  //     return ++acc;
+  //   } else {
+  //     return acc;
+  //   }
+  // }, 0); // here, 0 is the initiual number for the accumulator
 
-  const votesTopic1agegroupBetween35and44 = likes.reduce((acc, cur) => {
-    if (
-      listOfIdsPerTopic1.includes(cur.ideaId) &&
-      cur.age >= 35 &&
-      cur.age < 44
-    ) {
-      return ++acc;
-    } else {
-      return acc;
-    }
-  }, 0); // here, 0 is the initiual number for the accumulator
+  // const votesTopic1agegroupBetween35and44 = likes.reduce((acc, cur) => {
+  //   if (
+  //     listOfIdsPerTopic1.includes(cur.ideaId) &&
+  //     cur.age >= 35 &&
+  //     cur.age < 44
+  //   ) {
+  //     return ++acc;
+  //   } else {
+  //     return acc;
+  //   }
+  // }, 0); // here, 0 is the initiual number for the accumulator
 
-  const votesTopic1agegroupBetween45and54 = likes.reduce((acc, cur) => {
-    if (
-      listOfIdsPerTopic1.includes(cur.ideaId) &&
-      cur.age >= 45 &&
-      cur.age < 54
-    ) {
-      return ++acc;
-    } else {
-      return acc;
-    }
-  }, 0); // here, 0 is the initiual number for the accumulator
+  // const votesTopic1agegroupBetween45and54 = likes.reduce((acc, cur) => {
+  //   if (
+  //     listOfIdsPerTopic1.includes(cur.ideaId) &&
+  //     cur.age >= 45 &&
+  //     cur.age < 54
+  //   ) {
+  //     return ++acc;
+  //   } else {
+  //     return acc;
+  //   }
+  // }, 0); // here, 0 is the initiual number for the accumulator
 
-  const votesTopic1agegroupBetween55and64 = likes.reduce((acc, cur) => {
-    if (
-      listOfIdsPerTopic1.includes(cur.ideaId) &&
-      cur.age >= 55 &&
-      cur.age < 64
-    ) {
-      return ++acc;
-    } else {
-      return acc;
-    }
-  }, 0); // here, 0 is the initiual number for the accumulator
+  // const votesTopic1agegroupBetween55and64 = likes.reduce((acc, cur) => {
+  //   if (
+  //     listOfIdsPerTopic1.includes(cur.ideaId) &&
+  //     cur.age >= 55 &&
+  //     cur.age < 64
+  //   ) {
+  //     return ++acc;
+  //   } else {
+  //     return acc;
+  //   }
+  // }, 0); // here, 0 is the initiual number for the accumulator
 
-  const votesTopic1agegroupOver64 = likes.reduce((acc, cur) => {
-    if (
-      listOfIdsPerTopic1.includes(cur.ideaId) &&
-      cur.age >= 65 &&
-      cur.age < 104
-    ) {
-      return ++acc;
-    } else {
-      return acc;
-    }
-  }, 0); // here, 0 is the initiual number for the accumulator
+  // const votesTopic1agegroupOver64 = likes.reduce((acc, cur) => {
+  //   if (
+  //     listOfIdsPerTopic1.includes(cur.ideaId) &&
+  //     cur.age >= 65 &&
+  //     cur.age < 104
+  //   ) {
+  //     return ++acc;
+  //   } else {
+  //     return acc;
+  //   }
+  // }, 0); // here, 0 is the initiual number for the accumulator
 
-  const bike_array_with_14_values = [
-    -screams.filter(({ Thema, age }) => Thema === filteredTopic && age < 18)
-      .length,
-    votesTopic1agegroupUnder18,
+  const dataOutcome = [
+    -screams.filter(
+      ({ Thema, age }) => Thema === filteredTopic && age >= 14 && age < 18
+    ).length,
+    likes.filter(
+      ({ Thema, age }) => Thema === filteredTopic && age >= 14 && age < 18
+    ).length,
     -screams.filter(
       ({ Thema, age }) => Thema === filteredTopic && age >= 18 && age < 25
     ).length,
-    votesTopic1agegroupBetween18and24,
+    likes.filter(
+      ({ Thema, age }) => Thema === filteredTopic && age >= 18 && age < 25
+    ).length,
     -screams.filter(
       ({ Thema, age }) => Thema === filteredTopic && age >= 25 && age < 35
     ).length,
-    votesTopic1agegroupBetween25and34,
+    likes.filter(
+      ({ Thema, age }) => Thema === filteredTopic && age >= 25 && age < 35
+    ).length,
 
     -screams.filter(
       ({ Thema, age }) => Thema === filteredTopic && age >= 35 && age < 45
     ).length,
-    votesTopic1agegroupBetween35and44,
+    likes.filter(
+      ({ Thema, age }) => Thema === filteredTopic && age >= 35 && age < 45
+    ).length,
 
     -screams.filter(
       ({ Thema, age }) => Thema === filteredTopic && age >= 45 && age < 55
     ).length,
-    votesTopic1agegroupBetween45and54,
+    likes.filter(
+      ({ Thema, age }) => Thema === filteredTopic && age >= 45 && age < 55
+    ).length,
 
     -screams.filter(
       ({ Thema, age }) => Thema === filteredTopic && age >= 55 && age < 65
     ).length,
-    votesTopic1agegroupBetween55and64,
+    likes.filter(
+      ({ Thema, age }) => Thema === filteredTopic && age >= 55 && age < 65
+    ).length,
 
     -screams.filter(
       ({ Thema, age }) => Thema === filteredTopic && age >= 65 && age < 105
     ).length,
 
-    votesTopic1agegroupOver64,
+    likes.filter(
+      ({ Thema, age }) => Thema === filteredTopic && age >= 65 && age < 105
+    ).length,
   ];
-  return bike_array_with_14_values;
+  return dataOutcome;
 }
