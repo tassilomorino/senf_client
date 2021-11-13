b356369440e03885fd50a7a42e88734405b852ac
„tassilomorino“ <tassilomorino@gmail.com>
2021-11-13 12:39:09 +0100

    adding innerheight css --vh

--- src/App.js
+++ src/App.js
@@ -148,6 +148,10 @@ if (cookies.get("Cookie_settings") === "all") {
   }
 }
 
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
 const App = () => {
   const { t } = useTranslation();
 
