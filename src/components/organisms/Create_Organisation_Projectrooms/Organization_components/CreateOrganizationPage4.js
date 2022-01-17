af489c0d5f24889b9e8a39d37b5689d4fa38c8f7
„tassilomorino“ <tassilomorino@gmail.com>
2022-01-17 20:59:26 -0300

    enable adding moderators to organization

--- src/components/organisms/Create_Organisation_Projectrooms/Organization_components/CreateOrganizationPage4.js
+++ src/components/organisms/Create_Organisation_Projectrooms/Organization_components/CreateOrganizationPage4.js
@@ -1,6 +1,8 @@
 /** @format */
 
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+
+import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 
@@ -32,70 +34,142 @@ const SearchbarWrapper = styled.div`
   width: 100%;
 `;
 
-const Users = styled.div`
+const User = styled.div`
   width: 100%;
-
   height: 50px;
   border-radius: 50px;
   background-color: #353535;
   display: flex;
+  justify-content: space-between;
   align-items: center;
+  margin-top: 10px;
 `;
 
-const UserName = styled.p`
-  margin-left: 20px;
+const UserName = styled.div`
   color: white;
+  margin-left: 20px;
+`;
+
+const EditIcon = styled.div`
+  font-size: 20px;
+  margin-left: 0px;
+  color: white;
+  padding-right: 20px;
 `;
 
 const CreateOrganizationPage4 = ({ onClickNext, onClickPrev }) => {
   const { t } = useTranslation();
   const [nextClicked, setNextClicked] = useState(false);
 
+  const user = useSelector((state) => state.user);
+
   const [searchTerm, setSearchTerm] = useState("");
   const [authorizedUserIds, setAuthorizedUserIds] = useState(null);
   const [authorizedUserNames, setAuthorizedUserNames] = useState(null);
+  const [userList, setUsersList] = useState(null);
 
   const search = async (e) => {
+    const db = firebase.firestore();
     if (e.key === "Enter") {
+      const postRef = db.collection("users");
+      const users = [];
+      // define queries
+      const usersRef = await postRef
+        .orderBy("handle")
+        .startAt(searchTerm)
+        .endAt(searchTerm + "~")
+        .get();
+
+      // get queries
+      usersRef.docs.forEach((doc) => {
+        users.push(doc.data());
+
+        if (usersRef.size === users.length) {
+          setUsersList(users);
+        }
+      });
     }
   };
 
-  useEffect(() => {
-    async function fetchData() {
-      const db = firebase.firestore();
-      if (
-        typeof Storage !== "undefined" &&
-        localStorage.getItem("createOrganizationId")
-      ) {
-        const ref = await db
-          .collection("organizations")
-          .doc(localStorage.getItem("createOrganizationId"))
-          .get();
+  async function fetchData() {
+    const db = firebase.firestore();
+    if (
+      typeof Storage !== "undefined" &&
+      localStorage.getItem("createOrganizationId")
+    ) {
+      const ref = await db
+        .collection("organizations")
+        .doc(localStorage.getItem("createOrganizationId"))
+        .get();
 
-        if (!ref.exists) {
-          console.log("No such document!");
-        } else {
-          const data = ref.data();
+      if (!ref.exists) {
+        console.log("No such document!");
+      } else {
+        const data = ref.data();
 
-          if (data.userIds) {
-            const refUsers = await db
-              .collection("users")
-              .where("userId", "in", data.userIds)
-              .get();
+        if (data.userIds) {
+          const refUsers = await db
+            .collection("users")
+            .where("userId", "in", data.userIds)
+            .get();
 
-            const authorizedUserNamesRaw = [];
+          const authorizedUserNamesRaw = [];
 
-            refUsers.docs.forEach((doc) =>
-              authorizedUserNamesRaw.push({ ...doc.data() })
-            );
-            setAuthorizedUserNames(authorizedUserNamesRaw);
-            setAuthorizedUserIds(data.userIds);
-          }
+          refUsers.docs.forEach((doc) =>
+            authorizedUserNamesRaw.push({ ...doc.data() })
+          );
+          setAuthorizedUserNames(authorizedUserNamesRaw);
+          setAuthorizedUserIds(data.userIds);
         }
       }
     }
+  }
+
+  useEffect(() => {
     fetchData();
   }, []);
+
+  const handleRemove = async (userId) => {
+    if (userId === user.userId) {
+      alert("You can not remove yourself from the list of moderators");
+      return;
+    }
+    const db = firebase.firestore();
+
+    if (
+      typeof Storage !== "undefined" &&
+      localStorage.getItem("createOrganizationId")
+    ) {
+      const update = {
+        userIds: firebase.firestore.FieldValue.arrayRemove(userId),
+      };
+      const ref = await db
+        .collection("organizations")
+        .doc(localStorage.getItem("createOrganizationId"));
+      return ref.update(update).then(() => {
+        fetchData();
+      });
+    }
+  };
+
+  const handleAdd = async (userId) => {
+    const db = firebase.firestore();
+
+    if (
+      typeof Storage !== "undefined" &&
+      localStorage.getItem("createOrganizationId")
+    ) {
+      const update = {
+        userIds: firebase.firestore.FieldValue.arrayUnion(userId),
+      };
+      const ref = await db
+        .collection("organizations")
+        .doc(localStorage.getItem("createOrganizationId"));
+      return ref.update(update).then(() => {
+        fetchData();
+      });
+    }
+  };
 
   const handleNext = async () => {
     setNextClicked(true);
@@ -139,10 +213,11 @@ const CreateOrganizationPage4 = ({ onClickNext, onClickPrev }) => {
           </StyledH3>
           <br />
           {authorizedUserNames &&
-            authorizedUserNames.map((item) => (
-              <Users>
-                <UserName>{item.handle}</UserName>
-              </Users>
+            authorizedUserNames.map(({ handle, userId }) => (
+              <User onClick={() => handleRemove(userId)}>
+                <UserName>{handle}</UserName>
+                {userId !== user.userId && <EditIcon> -</EditIcon>}
+              </User>
             ))}
           <br /> <br />
           <SearchbarWrapper>
@@ -154,6 +229,13 @@ const CreateOrganizationPage4 = ({ onClickNext, onClickPrev }) => {
               backgroundColor="#f8f8f8"
             />
           </SearchbarWrapper>
+          {userList &&
+            userList.map(({ handle, userId }) => (
+              <User onClick={() => handleAdd(userId)}>
+                <UserName>{handle}</UserName>
+                <EditIcon> +</EditIcon>
+              </User>
+            ))}
           <br /> <br />
           <StyledH2 textAlign="center" fontWeight="900">
             Lade deine Teammitglieder:innen zu Senf ein
