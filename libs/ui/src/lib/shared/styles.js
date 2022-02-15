9b4790ae64bf32a32c53723023d24bc15b3a40a9
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-02-15 15:12:55 +0100

    add potiantial shared styles

---
+++ libs/ui/src/lib/shared/styles.js
@@ -0,0 +1,53 @@
+import { css } from "@storybook/theming";
+import { rgba } from "polished";
+
+// Global style variables
+export const background = {
+  positive: "#",
+  negative: "#",
+  warning: "#",
+};
+
+export const color = {
+  // Palette
+  primary: "#",
+  secondary: "#",
+  tertiary: "#",
+};
+
+export const spacing = {
+  padding: {
+    small: 10,
+    medium: 20,
+    large: 30,
+  },
+  borderRadius: {
+    small: 5,
+    default: 10,
+  },
+};
+
+export const typography = {
+  type: {
+    primary: '"Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif',
+    code: '"SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace',
+  },
+  weight: {
+    regular: "400",
+    bold: "700",
+    extrabold: "800",
+    black: "900",
+  },
+  size: {
+    s1: 12,
+    s2: 14,
+    s3: 16,
+    m1: 20,
+    m2: 24,
+    m3: 28,
+    l1: 32,
+    l2: 40,
+    l3: 48,
+    code: 90,
+  },
+};
