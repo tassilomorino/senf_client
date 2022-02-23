ce786615fed3a2fe4e550cb4fc608271c003a0cc
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-02-23 11:43:40 +0100

    add vite-plugin-pwa

--- apps/senf-monitoring/src/index.jsx
+++ apps/senf-monitoring/src/index.jsx
@@ -3,7 +3,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+//import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { registerSW } from "virtual:pwa-register";
+
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
@@ -13,7 +15,10 @@ ReactDOM.render(
   rootElement
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorkerRegistration.register();
+//serviceWorkerRegistration.register(); old SW
+
+registerSW({
+  onOfflineReady() {
+    console.log("ready for offline use");
+  },
+});
