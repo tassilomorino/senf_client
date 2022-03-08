036940fcdeb663b0d2c5d25ed673bedc21bc482f
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-03-08 11:44:38 +0100

    fix full page reload HMR problem

--- apps/senf-workspace/vite.config.js
+++ apps/senf-workspace/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import reactJsx from "vite-react-jsx";
-
+import reactRefresh from "@vitejs/plugin-react-refresh";
 export default defineConfig({
-  plugins: [reactJsx()],
+  plugins: [reactJsx(), reactRefresh()],
 });
