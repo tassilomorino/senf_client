104fe143f946bba695b8a9fabc1e71761f18a99e
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-03-08 15:25:50 +0100

    remove legacy vite plugin

--- apps/senf-workspace/vite.config.js
+++ apps/senf-workspace/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
-import reactJsx from "vite-react-jsx";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
-  plugins: [reactJsx(), reactRefresh()],
+  plugins: [react()],
 });
