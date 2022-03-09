9e109e2a6afc4f60624287d818b59152ca855b7c
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-03-09 15:41:22 +0100

    add react strict mode for development

--- apps/senf-client/src/index.jsx
+++ apps/senf-client/src/index.jsx
@@ -3,9 +3,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 const rootElement = document.getElementById("root");
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
 // will need to add vite PWA plugin
 /* serviceWorkerRegistration.register(); */
