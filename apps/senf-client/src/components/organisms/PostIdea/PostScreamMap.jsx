c2c5e4729627dee0f4acac594bad8182d915cebe
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-03-10 13:39:08 +0100

    add geocoder to our app

--- apps/senf-client/src/components/organisms/PostIdea/PostScreamMap.jsx
+++ apps/senf-client/src/components/organisms/PostIdea/PostScreamMap.jsx
@@ -5,6 +5,7 @@ import { isMobileCustom } from "../../../util/customDeviceDetect";
 import MapGL, { GeolocateControl, Source, Layer } from "@urbica/react-map-gl";
 
 //import Geocoder from "react-mapbox-gl-geocoder";
+import Geocoder from "../../atoms/map/Geocoder";
 
 import { useTranslation } from "react-i18next";
 
@@ -147,7 +148,7 @@ const PostScreamMap = ({
               : { display: "none" }
           }
         >
-          {/* <Geocoder
+          <Geocoder
             mapboxApiAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
             onSelected={onSelected}
             viewport={viewport}
@@ -156,7 +157,7 @@ const PostScreamMap = ({
             queryParams={queryParams}
             id="geocoder"
             transitionDuration={1000}
-          /> */}
+          />
           <AdressLinePlaceHolderDiv
             style={clicked === false ? { zIndex: 9999 } : { zIndex: 0 }}
           >
