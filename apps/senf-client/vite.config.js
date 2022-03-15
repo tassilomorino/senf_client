0ff4abba7ac58ad2d05be2b3ffd2221ff8e0bdce
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-03-15 15:25:28 +0100

    add bundle size visualizer

--- apps/senf-client/vite.config.js
+++ apps/senf-client/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+/* import { visualizer } from "rollup-plugin-visualizer"; */
+// npm run build will create a file stats.html in root directory
 
 export default defineConfig(({ command, mode }) => {
   return {
-    plugins: [react()],
+    plugins: [react() /* , visualizer({ gzipSize: true, brotliSize: true } )*/],
   };
 });
