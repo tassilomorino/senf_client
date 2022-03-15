3b718b605e5bd50ae253b3627100c7e2b21f7a23
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-03-15 09:30:55 +0100

    remove imports

--- apps/senf-client/vite.config.js
+++ apps/senf-client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { esbuildCommonjs, viteCommonjs } from "@originjs/vite-plugin-commonjs";
 
 export default defineConfig(({ command, mode }) => {
   return {
