ec3fb4119d72206c7ede80dad426dcb3a07e7667
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-04-08 15:08:36 +0200

    add React.strictmode

--- apps/senf-client/src/index.jsx
+++ apps/senf-client/src/index.jsx
@@ -7,7 +7,11 @@ import App from "./App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 /* registerSW({
   onOfflineReady() {
