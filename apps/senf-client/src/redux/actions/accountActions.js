fcfe8dc09ac9e937329761f287327b3052d87b8d
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-05-13 16:22:44 +0100

    rewrite accountActions.js

--- apps/senf-client/src/redux/actions/accountActions.js
+++ apps/senf-client/src/redux/actions/accountActions.js
@@ -1,9 +1,8 @@
 /** @format */
 
-import firebase from "firebase/compat/app";
-import "firebase/compat/firestore";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 import setColorByTopic from "../../data/setColorByTopic";
-
+import { db } from "../../firebase";
 import {
   SET_MY_SCREAMS,
   SET_MY_ORGANIZATIONS,
@@ -21,68 +20,57 @@ export const openAccountFunc = () => async (dispatch) => {
 
 export const getMyScreams = (userId) => async (dispatch) => {
   dispatch({ type: LOADING_MYSCREAMS_DATA });
-  const db = firebase.firestore();
-  const ref = await db
-    .collection("screams")
-    .where("userId", "==", userId)
-    .orderBy("createdAt", "desc")
-    .get();
-
   const screams = [];
-
-  ref.docs?.forEach((doc) => {
-    const docData = {
-      screamId: doc.id,
-      lat: doc.data().lat,
-      long: doc.data().long,
-      title: doc.data().title,
-      body: doc.data().body.substr(0, 170),
-      createdAt: doc.data().createdAt,
-      commentCount: doc.data().commentCount,
-      likeCount: doc.data().likeCount,
-      status: doc.data().status,
-      Thema: doc.data().Thema,
-      Stadtteil: doc.data().Stadtteil,
-      project: doc.data().project,
-      projectId: doc.data().project,
-      color: setColorByTopic(doc.data().Thema),
-    };
-
-    screams.push(docData);
-  });
-
-  dispatch({
-    type: SET_MY_SCREAMS,
-    payload: screams,
-  });
+  const screamsRef = collection(db, "screams");
+  const q = query(
+    screamsRef,
+    where("userId", "==", userId),
+    orderBy("createdAt", "desc")
+  );
+  const screamsSnapshot = await getDocs(q);
+  if (screamsSnapshot.empty) {
+    dispatch({
+      type: SET_MY_SCREAMS,
+      payload: screams,
+    });
+  } else {
+    screamsSnapshot.forEach((doc) => {
+      screams.push({
+        ...doc.data(),
+        screamId: doc.id,
+        body: doc.data().body.substring(0, 170) + "...",
+        color: setColorByTopic(doc.data().topic),
+      });
+    });
+    dispatch({
+      type: SET_MY_SCREAMS,
+      payload: screams,
+    });
+  }
 };
 export const getMyOrganizations = (userId) => async (dispatch) => {
   dispatch({ type: LOADING_MYORGANIZATIONS_DATA });
-  const db = firebase.firestore();
-  const ref = await db
-    .collection("organizations")
-    // .where("centerLat", "<", Number(mapViewport.latitude) + 1)
-    // .where("centerLat", ">", Number(mapViewport.latitude) - 1)
-    .orderBy("createdAt", "desc")
-    .where("userIds", "array-contains", userId)
-    .get();
-
   const organizations = [];
-
-  ref.docs.forEach((doc) => {
-    const docData = {
-      ...doc.data(),
-      organizationId: doc.id,
-    };
-    organizations.push(docData);
-
-    if (organizations.length === ref.size) {
-      dispatch({
-        type: SET_MY_ORGANIZATIONS,
-        payload: organizations,
+  const organizationsRef = collection(db, "organizations");
+  const q = query(organizationsRef, where("userIds", "array-contains", userId));
+  const organizationsSnapshot = await getDocs(q);
+  if (organizationsSnapshot.empty) {
+    dispatch({
+      type: SET_MY_ORGANIZATIONS,
+      payload: organizations,
+    });
+  } else {
+    organizationsSnapshot.forEach((doc) => {
+      organizations.push({
+        ...doc.data(),
+        organizationId: doc.id,
       });
-    }
-  });
+    });
+    dispatch({
+      type: SET_MY_ORGANIZATIONS,
+      payload: organizations,
+    });
+  }
 };
 
 export const closeAccountFunc = () => async (dispatch) => {
