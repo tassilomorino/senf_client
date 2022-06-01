da179cafd61584dab835685316dd5ee6c25efbdc
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-06-01 15:49:25 +0200

    add instructions to delete account

--- apps/senf-client/src/components/organisms/infocomponents/legal/LegalStyles.jsx
+++ apps/senf-client/src/components/organisms/infocomponents/legal/LegalStyles.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { CustomIconButton } from "../../../atoms/CustomButtons/CustomButton";
 import styled from "styled-components";
 import LogoWhite from "../../../../images/logo_white.png";
@@ -11,50 +11,53 @@ const Wrapper = styled.div`
   margin: auto;
   max-width: 600px;
   margin-top: 30vh;
-`
+
+  li {
+    font-size: 12px;
+  }
+`;
 const Logo = styled.h1`
   position: relative;
   top: 0em;
   left: 0vw;
-`
+`;
 
 export const LegalStyles = ({ children }) => {
+  const linkToHome = () => {
+    window.location.href = "/";
+  };
 
-    const linkToHome = () => {
-        window.location.href = "/";
-    }
+  return (
+    <div>
+      <CustomIconButton
+        name="Close"
+        position="fixed"
+        margin={document.body.clientWidth > 768 ? "40px" : "10px"}
+        left="0"
+        handleButtonClick={() => linkToHome()}
+        top="0"
+      />
 
-    return (
-        <div>
-            <CustomIconButton
-                name="Close"
-                position="fixed"
-                margin={document.body.clientWidth > 768 ? "40px" : "10px"}
-                left="0"
-                handleButtonClick={() => linkToHome()}
-                top="0"
-            />
-
-            <Wrapper>
-                <Logo onClick={() => linkToHome()}>
-                    <img src={LogoWhite} width="100px" alt="logo" />
-                </Logo>
-                {children}
-            </Wrapper>
-        </div>
-    )
-}
+      <Wrapper>
+        <Logo onClick={() => linkToHome()}>
+          <img src={LogoWhite} width="100px" alt="logo" />
+        </Logo>
+        {children}
+      </Wrapper>
+    </div>
+  );
+};
 
 const TermsWrapper = styled.a`
   text-decoration: underline;
   color: #414345;
   cursor: pointer;
-`
+`;
 
 export const Terms = ({ children, href, target, rel }) => {
-    return (
-        <TermsWrapper prefetch href={href} passHref target={target} rel={rel}>
-            {children}
-        </TermsWrapper>
-    )
-}
+  return (
+    <TermsWrapper prefetch href={href} passHref target={target} rel={rel}>
+      {children}
+    </TermsWrapper>
+  );
+};
