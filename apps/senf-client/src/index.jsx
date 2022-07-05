332aa10bb076eab9dc0bb6bdca38adbee49f2a90
Digital-Coder <31192965+Digital-Coder@users.noreply.github.com>
2022-07-05 19:40:28 +0300

    test

--- apps/senf-client/src/index.jsx
+++ apps/senf-client/src/index.jsx
@@ -5,7 +5,7 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 // import { registerSW } from "virtual:pwa-register";
-//
+
 ReactDOM.render(
   <React.StrictMode>
     <Suspense fallback="">
