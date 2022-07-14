bf8e7313d3e42d871f7da5168cb498ea0d078b89
„tassilomorino“ <tassilomorino@gmail.com>
2022-07-14 14:19:16 +0200

    add new wave

--- libraries/senf-atomic-design-system/src/components/atoms/shapes/Wave.tsx
+++ libraries/senf-atomic-design-system/src/components/atoms/shapes/Wave.tsx
@@ -1,10 +1,13 @@
-import React, { memo } from "react";
+import React, { memo, useEffect, useRef } from "react";
 import styled from "styled-components";
+import lottie from "lottie-web";
+import wave from "../../../assets/lottieFiles/senf-wave.json";
 
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   position: absolute;
+  top: 0;
   overflow: hidden;
   top: ${({ top }) => top && top};
   position: ${({ position }) => position || "absolute"};
@@ -15,17 +18,41 @@ const Wrapper = styled.div`
 
 const Block = styled.div`
   display: block;
+  position: relative;
   width: 150%;
   height: 1570px;
   opacity: 1;
   background-color: ${({ color, theme }) =>
     color || theme.colors.beige.beige20};
 `;
-const Wave = ({ position, top, color }) => (
-  <Wrapper position={position} top={top}>
-    <div style={{ width: "500px", height: "300px" }}></div>
-    <Block id="wave" color={color} />
-  </Wrapper>
-);
+const Wave = ({ position, top, color }) => {
+  const container = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: wave,
+    });
+  }, []);
+
+  return (
+    <Wrapper position={position} top={top}>
+      <div
+        style={{
+          width: "500px",
+          height: "200px",
+          position: "absolute",
+          top: "-60px",
+          zIndex: 99999,
+        }}
+        ref={container}
+      ></div>
+      <Block id="wave" color={color} />
+    </Wrapper>
+  );
+};
 
 export default memo(Wave);
