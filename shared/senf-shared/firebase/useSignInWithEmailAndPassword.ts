bba404c998fcbb26a48093675a3407179e8de040
Digital-Coder <31192965+Digital-Coder@users.noreply.github.com>
2022-07-20 13:00:32 +0200

    test default hook

--- shared/senf-shared/firebase/useSignInWithEmailAndPassword.ts
+++ shared/senf-shared/firebase/useSignInWithEmailAndPassword.ts
@@ -8,7 +8,7 @@ import { useState, useMemo } from "react";
 import { EmailAndPasswordActionHook } from "./types";
 import { generateErrorMessage } from "./generateErrorMessage";
 
-export const useSignInWithEmailAndPassword = (
+/* export const useSignInWithEmailAndPassword = (
   auth: Auth
 ): EmailAndPasswordActionHook => {
   const [error, setError] = useState({ code: "", message: "" });
@@ -46,6 +46,73 @@ export const useSignInWithEmailAndPassword = (
         code: err.code,
         message: generateErrorMessage(err.code),
       });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const resArray: EmailAndPasswordActionHook = [
+    signInWithEmailAndPassword,
+    loggedInUser,
+    loading,
+    error,
+  ];
+  return useMemo<EmailAndPasswordActionHook>(() => resArray, resArray);
+}; */
+
+/* export const handleSubmitLogin = async (formikLoginStore) => {
+  // event.preventDefault();
+
+  setLoading(true);
+  signInWithEmailAndPassword(
+    auth,
+    formikLoginStore.values.email,
+    formikLoginStore.values.password
+  )
+    .then((userCredential) => {
+      if (userCredential.user.emailVerified) {
+        console.log(userCredential.user.uid);
+        setLoading(false);
+        dispatch({ type: SET_AUTHENTICATED });
+        dispatch(getUserData(userCredential.user.uid));
+        console.log(window.location);
+
+        setAuthOpen(false);
+      } else {
+        setLoading(false);
+        setErrorMessage(t("email_not_verified"));
+      }
+    })
+    .catch((err) => {
+      setLoading(false);
+      setErrorMessage(err.message);
+    });
+
+  // dispatch(loginUser(userData, props.history))
+}; */
+
+export const useSignInWithEmailAndPassword = (
+  auth: Auth
+): EmailAndPasswordActionHook => {
+  const [error, setError] = useState<AuthError>();
+  const [loggedInUser, setLoggedInUser] = useState<UserCredential>();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const signInWithEmailAndPassword = async (
+    email: string,
+    password: string
+  ) => {
+    setLoading(true);
+    setError(undefined);
+    try {
+      const user = await firebaseSignInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      setLoggedInUser(user);
+    } catch (err) {
+      setError(err as AuthError);
     } finally {
       setLoading(false);
     }
