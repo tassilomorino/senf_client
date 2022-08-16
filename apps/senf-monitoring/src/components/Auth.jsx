0e3840e7c94304cd0534475d46abbeb6a705b5b3
Digital-Coder <31192965+Digital-Coder@users.noreply.github.com>
2022-08-16 17:13:46 +0200

    Revert "test custom verify url"
    This reverts commit 81ca41f54ffcf8a683063a9605014c792e77ea83.

--- apps/senf-monitoring/src/components/Auth.jsx
+++ apps/senf-monitoring/src/components/Auth.jsx
@@ -77,7 +77,7 @@ const Auth = ({ setAuthOpen, setAuthEditOpen, authOpen, authEditOpen }) => {
   const sendVerification = {
     sendEmailVerification: true,
     emailVerificationOptions: {
-      url: "https://deploy-preview-544--senf-client-test.netlify.app/",
+      url: "https://senf.koeln/verify",
     },
   };
   const [
