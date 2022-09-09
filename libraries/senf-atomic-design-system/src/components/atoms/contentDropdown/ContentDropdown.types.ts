67bb6644690d9664b0c404a0c6b1278c1537c79f
Digital-Coder <31192965+Digital-Coder@users.noreply.github.com>
2022-09-09 10:25:42 +0200

    contentDropdown types

--- libraries/senf-atomic-design-system/src/components/atoms/contentDropdown/ContentDropdown.types.ts
+++ libraries/senf-atomic-design-system/src/components/atoms/contentDropdown/ContentDropdown.types.ts
@@ -5,10 +5,10 @@ import React, { MouseEventHandler } from "react";
 export interface ContentDropdownProps {
   example?: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
-  OpenButton: React.ReactNode;
-  Content: React.ReactNode;
-  openButtonWidth: string;
-  open: boolean;
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  OpenButton?: React.ReactNode;
+  Content?: React.ReactNode;
+  openButtonWidth?: string;
+  open?: boolean;
+  setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
   direction?: "downLeft" | "downRight";
 }
