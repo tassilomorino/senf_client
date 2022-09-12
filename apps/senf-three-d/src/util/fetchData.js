e82dab4f253fda4265ff9f04f393a5b148c72d7b
tassilomorino <tassilomorino@hotmail.de>
2022-09-12 12:48:29 +0200

    3D-tool:start implementing correct saving process

--- apps/senf-three-d/src/util/fetchData.js
+++ apps/senf-three-d/src/util/fetchData.js
@@ -1,14 +1,7 @@
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../firebase";
-
 export async function fetchData() {
-  const ref = doc(db, "threeD_ideas", "GS4MT3HDv3YcojewHeLx");
-  const docSnapshot = await getDoc(ref);
-  if (!docSnapshot.exists()) {
-    console.log("No such document!");
-    return null;
-  }
-  const data = docSnapshot.data();
+  const data = {
+    drawnData: localStorage.getItem("drawnData"),
+    modelsData: localStorage.getItem("modelsData"),
+  };
   return data;
-  // formik.setFieldValue("title", data.title);
 }
