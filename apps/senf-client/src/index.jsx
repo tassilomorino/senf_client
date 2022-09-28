bdd3e824e0338c8398970ec61187328961c0329d
Digital-Coder <31192965+Digital-Coder@users.noreply.github.com>
2022-09-28 12:04:22 +0200

    try to fix png bug
    rem images
    
    add back images

--- apps/senf-client/src/index.jsx
+++ apps/senf-client/src/index.jsx
@@ -5,6 +5,7 @@ import React, { Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 // import { registerSW } from "virtual:pwa-register";
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
