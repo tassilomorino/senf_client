8024c6058baa16588543615a0a107526e04477b6
Digital-Coder <31192965+Digital-Coder@users.noreply.github.com>
2022-10-03 15:12:29 +0200

    improve socialmediashare design

--- libraries/senf-atomic-design-system/src/components/organisms/socialmediaShare/SocialmediaShare.tsx
+++ libraries/senf-atomic-design-system/src/components/organisms/socialmediaShare/SocialmediaShare.tsx
@@ -2,24 +2,29 @@
 
 import React, { FC } from "react";
 import styled from "styled-components";
-import { SocialmediaShareProps } from "./SocialmediaShare.types";
 import { useTranslation } from "react-i18next";
+import {
+  WhatsappShareButton,
+  FacebookShareButton,
+  EmailShareButton,
+} from "react-share";
+import { SocialmediaShareProps } from "./SocialmediaShare.types";
 import Typography from "../../atoms/typography/Typography";
 import TertiaryButton from "../../atoms/buttons/TertiaryButton";
 import Facebook from "../../../assets/icons/Facebook";
 import Mail from "../../../assets/icons/Mail";
 import Box from "../../atoms/box/Box";
 import WhatsApp from "../../../assets/icons/WhatsApp";
-import {
-  WhatsappShareButton,
-  FacebookShareButton,
-  EmailShareButton,
-} from "react-share";
-const Wrapper = styled.div<SocialmediaShareProps>``;
+
+const Wrapper = styled.div<SocialmediaShareProps>`
+display: flex;
+flex-direction: column;
+gap:20px;
+margin:10px 5px 10px;
+`;
 
 const SocialmediaShare: FC<SocialmediaShareProps> = ({
   path,
-  handleShareIdeaVia,
 }) => {
   const { t } = useTranslation();
   return (
@@ -27,8 +32,7 @@ const SocialmediaShare: FC<SocialmediaShareProps> = ({
       <Typography variant="h3"> {t("share_link")}</Typography>
       <Box
         flexDirection="column"
-        gap="5px"
-        margin="8px 0px 0px 0px"
+        gap="15px"
         justifyContent="flex-start"
         alignItems="flex-start"
       >
